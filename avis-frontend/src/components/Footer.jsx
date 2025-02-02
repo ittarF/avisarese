b0914b62b53d119abc8a75a3fa80fd72325ae52c
Giorgio Fratti <ittarf@gmail.com>
2025-02-02 23:30:07 +0100

    bigger footer

--- avis-frontend/src/components/Footer.jsx
+++ avis-frontend/src/components/Footer.jsx
@@ -4,28 +4,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-    return (
-        <footer className="bg-red-600 text-white">
-            <div className="flex font-semibold text-sm items-center justify-center">
-                <div className="text-center">
-                    <img
-                        src={logo}
-                        alt="AVIS Arese"
-                        className="h-8 mx-auto"
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <h1>Comunale</h1>
-                    <h1>Arese ODV</h1>
-                </div>
-                <div className="text-center pl-8">
-                    <p><FontAwesomeIcon icon={faPhone} size="sm" /> +39 345 362 0799 - <FontAwesomeIcon icon={faEnvelope} size="sm" /> avis.arese@gmail.com</p>
-                </div>
-            </div>
-        </footer>
-    );
-}
+  return (
+    <footer className="bg-red-600 text-gray-300 p-6 text-sm">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 text-center md:text-left">
+        <div>
+          <h3 className="text-white font-bold">Contatti</h3>
+          <p>Cell: +39 345 362 0799</p>
+          <p>Mail: <a href="mailto:aavis.arese@gmail.com" className="text-blue-400">avis.arese@gmail.com</a></p>
+        </div>
+        <div>
+          <h3 className="text-white font-bold">Dove siamo</h3>
+          <p>Via Caduti, 2</p>
+          <p>20044 Arese</p>
+        </div>
+        <div>
+          <h3 className="text-white font-bold">Orari apertura Sede</h3>
+          <p>Martedì: 09.00 - 12.30</p>
+          <p>Giovedì: 09.00 - 12.30</p>
+        </div>
+      </div>
 
-        
-  export default Footer;
+      <div className="flex items-center justify-between mt-6 border-t border-white pt-4">
+        <img src={logo} alt="Logo AVIS" className="w-24" />
+        <p className="flex-1 text-center">PRIVACY POLICY E INFORMATIVE</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
   
